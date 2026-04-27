994310cff528e7fbbb546cbb2ef33fd5f1b1f898
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-04-27 16:49:12 +0200

    terminato selfwork php_1

--- file.php
+++ file.php
@@ -1,3 +1,19 @@
 <?php
+// creazione variabili
+$Integer = 5;
+$Float = 2.5;
+$String = "ciao";
+$Boolean = true;
+
+var_dump ($Integer);
+var_dump ($Float);
+var_dump ($String);
+var_dump ($Boolean);
+
+// tarsformazione variabili in costanti
+const INTERO = 5;
+const DECIMALE = 2.5;
+const STRINGA = "ciao";
+const BOOLEANO = true;
 
 ?>
